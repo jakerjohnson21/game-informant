caad1258f2b0f83f28b1c567505e2ceee8ba8be0
jakerjohnson21 <jakekjohnson@outlook.com>
2020-03-25 14:05:07 -0700

    Slight fixes to home page

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -3,4 +3,36 @@ console.log('home view');
 $(document).ready (function () {
     console.log('home view');
     console.log($.ajax);
+
+
+    $.ajax({
+        method: "GET",
+        url: "https://api.rawg.io/api/games",
+        data: {
+          page_size: 10
+        },
+        success: function (response) {
+          console.log(response);
+        },
+        error: function (thrownError) {
+          console.log(thrownError);
+        }
+      });
+
+    //   $.ajax({
+    //     method: "GET",
+    //     url: "https://api.rawg.io/api/games/3498/twitch",
+    //     data: {
+    //       page_size: 10
+    //     },
+    //     success: function (response) {
+    //       console.log(response);
+    //     },
+    //     error: function (thrownError) {
+    //       console.log(thrownError);
+    //     }
+    //   });
+
+
+
 });
