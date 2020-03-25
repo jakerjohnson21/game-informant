90e06bbad41c0478ef7b038815fef467beefd672
ajay-k <ajay_k@sbcglobal.net>
2020-03-25 14:02:12 -0700

    Adds ajax call and dom elements to home page, intial routes done

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,6 +1,27 @@
 console.log('home view');
 
 $(document).ready (function () {
-    console.log('home view');
-    console.log($.ajax);
+    $.ajax({
+        method: "GET",
+        url: "https://api.rawg.io/api/games?dates=2020-01-01,2020-10-10&ordering=-added",
+        data: {
+          page_size: 16
+        },
+        success: function (response) {
+          console.log(response);
+
+          for(let i=0; i < response.results.length; i++){
+          	$(`.games-grid-container`).
+          	append(
+          	`<div class="game-card" id="game-card-${response.results[i].id}">
+                           <img src="${response.results[i].background_image}">
+                           <div class="game-card-title">${response.results[i].name}</div>
+          	</div>`);
+          }
+          
+        },
+        error: function (thrownError) {
+          console.log(thrownError);
+        }
+      });
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // SERVER CONFIG
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const DB_URI = process.env.MONGODB || `mongodb://localhost:27017/game-informant`;
@@ -9,7 +10,76 @@ const DB_URI = process.env.MONGODB || `mongodb://localhost:27017/game-informant`
 
 app.use(bodyParser.json());
 
+app.use(express.static(`${__dirname}/public`));
+
 const db = require(`./models`);
+
+//const Game = mongoose.model(`Game`, GameSchema);
+
+// ------------HTML ROUTES------------ //
+
+// Login screen
+app.get(`/`, (req,res) => {
+	console.log(__dirname);
+	res.sendFile(`views/login.html`, {
+		root: __dirname,
+	});
+});
+
+// Home page
+app.get(`/home`, (req,res) => {
+	res.sendFile(`views/home.html`, {
+		root: __dirname,
+	});
+});
+
+// Profile Page
+app.get(`/profile/:id`, (req,res) => {
+	res.sendFile(`views/profile.html`, {
+		root: __dirname,
+	});
+});
+
+// Specifc Game Info
+app.get(`/games/:id`, (req,res) => {
+	res.sendFile(`views/games/gamesShow.html`, {
+		root: __dirname,
+	});
+});
+
+// Redirect to homes if no id is present
+app.get(`/games`, (req,res) => {
+	res.sendFile(`views/home.html`, {
+		root: __dirname,
+	});
+});
+
+//------------API ROUTES------------ //
+
+app.get(`/api/v1/games`, (req,res) => {
+	db.Game.find({}, (err, allGames) => {
+		if(err){
+			return res.status(400).json({status: 400,
+			 error: "Sorry something went wrong, please try again"});
+		}
+		res.json(allGames);
+	});
+});
+
+// Returns json object of specific game if found
+app.get(`/api/v1/games/:id`, (req,res) => {
+	db.Game.findById(req.params.id, (err, foundGame) => {
+		if(err){
+			console.log(`ID: `, req.params.id);
+			return res.status(400).json({status: 400,
+			error: "Sorry something went wrong, please try again"});
+		}
+
+		res.json(foundGame);
+	});
+});
+
+
 
 
 
