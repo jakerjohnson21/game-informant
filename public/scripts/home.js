150c53a2acfb1627893e538c503f7f5fe75d7bd6
ajay-k <ajay_k@sbcglobal.net>
2020-03-25 16:57:33 -0700

    Adds popover for single card element

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,6 +1,15 @@
 console.log('home view');
 
 $(document).ready (function () {
+		
+		$("[data-toggle=popover]").popover();
+		// $('#game-card-41494').popover(options)
+		// data-content="This is the body of Popover"
+  //  data-original-title="Creativity Tuts" rel="popover"
+
+		 // <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" 
+		 // data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
+
     $.ajax({
         method: "GET",
         url: "https://api.rawg.io/api/games?dates=2020-01-01,2020-10-10&ordering=-added",
@@ -9,15 +18,16 @@ $(document).ready (function () {
         },
         success: function (response) {
           console.log(response);
-
           for(let i=0; i < response.results.length; i++){
           	$(`.games-grid-container`).
           	append(
-            `<div class="game-card" id="game-card-${response.results[i].id}">
+            `<div data-content="<button>Favorite</button>" data-original-title="${response.results[i].name}" rel="popover" class="game-card" id="game-card-${response.results[i].id}"  >
                           <img src="${response.results[i].background_image}">
                           <div class="game-card-title">${response.results[i].name}</div>
           	</div>`);
           }
+          $('#game-card-41494').popover('show');
+
         },
         error: function (thrownError) {
           console.log(thrownError);
